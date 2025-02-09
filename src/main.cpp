8058fbc0426d15a61cb4b57a48b3cb309143d490
suads <suads.smajlovic14@gmail.com>
2025-02-09 20:35:36 +0100

    add Include

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+#include <filesystem>
 
-
-int main(){
-    
+int main(int argc, char* argv[])
+{
+  if(argc > 1){
+    std::filesystem::path fileToCompile = argv[1];
+    if(std::filesystem::exists(fileToCompile)){
+    }
+    else{
+      std::cerr << "Error: File does not exist!" << std::endl;
+    }
+  }
+  else{
+     std::cerr << "Error: Please specify a file to compile!" << std::endl;
+  }
 }
